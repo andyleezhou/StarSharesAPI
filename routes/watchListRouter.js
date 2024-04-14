8a80eab0da811816185ca9a762114fa423fef52b
omshah11 <61638057+omshah11@users.noreply.github.com>
2024-04-14 13:51:04 -0700

    Added deleteWatchlist endpoint

--- routes/watchListRouter.js
+++ routes/watchListRouter.js
@@ -4,54 +4,93 @@ const mongoose = require("mongoose");
 const logger = require("../config/logger");
 const WatchList = require("../model/watchListSchema");
 
-
 /// Watchlist Stock Endpoint
 
 router.post("/createWatchlist", async (request, response) => {
-    const { userId } = request.body;
+  const { userId } = request.body;
 
-    
-  
-    if (!userId) {
-      logger.error("User ID cannot be null");
-      return response.status(400).json({
-        message: "User ID cannot be null",
-        status: 400,
+  if (!userId) {
+    logger.error("User ID cannot be null");
+    return response.status(400).json({
+      message: "User ID cannot be null",
+      status: 400,
+    });
+  }
+
+  // Check if watchlist already exists for the user
+  try {
+    const userObjId = new mongoose.Types.ObjectId(userId);
+    let watchlist = await WatchList.findOne({ userId: userObjId }).exec();
+    if (watchlist) {
+      logger.error("Watchlist already exists");
+      return response.status(409).json({
+        message: "Watchlist already exists",
+        status: 409,
       });
     }
-  
-    // Check if watchlist already exists for the user
-    try {
-      const userObjId = new mongoose.Types.ObjectId(userId);
-      let watchlist = await WatchList.findOne({userId: userObjId}).exec();
-      if (watchlist) {
-        logger.error("Watchlist already exists");
-        return response.status(409).json({
-          message: "Watchlist already exists",
-          status: 409,
-        });
-      }
-  
-      // Create a new watchlist for the user
-      watchlist = new WatchList(request.body);
-  
-      logger.info("Attempting to save new Watchlist to MongoDB");
-      await watchlist.save();
-      logger.info("Watchlist saved");
-      return response.status(201).json({
-        message: "Watchlist successfully created",
-        status: 201,
-        watchlist: watchlist,
-      });
-    } catch (error) {
-      return response.status(500).json({
-        message: "Failed to create a new watchlist",
-        status: 500,
-        error: error.message,
+
+    // Create a new watchlist for the user
+    watchlist = new WatchList(request.body);
+
+    logger.info("Attempting to save new Watchlist to MongoDB");
+    await watchlist.save();
+    logger.info("Watchlist saved");
+    return response.status(201).json({
+      message: "Watchlist successfully created",
+      status: 201,
+      watchlist: watchlist,
+    });
+  } catch (error) {
+    return response.status(500).json({
+      message: "Failed to create a new watchlist",
+      status: 500,
+      error: error.message,
+    });
+  }
+});
+
+router.post("/deleteWatchlist", async (request, response) => {
+  const { userId } = request.body;
+
+  if (!userId) {
+    logger.error("User ID cannot be null");
+    return response.status(400).json({
+      message: "User ID cannot be null",
+      status: 400,
+    });
+  }
+
+  const userObjId = new mongoose.Types.ObjectId(userId);
+
+  try {
+    // Find the watchlist by userId
+    logger.info("Attempting to find watchlist to delete");
+    let watchlist = await WatchList.findOne({ userId: userObjId });
+    if (!watchlist) {
+      logger.error("Watchlist not found");
+      return response.status(404).json({
+        message: "Wathclist not found",
+        status: 404,
       });
     }
-  });
-  
+
+    // Delete watchlist
+    logger.info("Found watchlist to be deleted");
+    console.log(watchlist);
+    await WatchList.deleteOne({ userId: userObjId });
+    logger.info("Watchlist successfully deleted");
+    return response.status(200).json({
+      message: "Watchlist successfully deleted",
+      status: 200,
+    });
+  } catch (error) {
+    return response.status(500).json({
+      message: "Falied to delete watchlist",
+      status: 500,
+      error: error.message,
+    });
+  }
+});
 
 router.post("/addToWatchlist", async (request, response) => {
   const { userId, stockId } = request.body;
@@ -69,7 +108,7 @@ router.post("/addToWatchlist", async (request, response) => {
 
   try {
     // Find the watchlist by userId
-    let watchlist = await WatchList.findOne({userId: userObjId});
+    let watchlist = await WatchList.findOne({ userId: userObjId });
     if (!watchlist) {
       logger.error("Watchlist not found");
       return response.status(404).json({
@@ -156,7 +195,7 @@ router.post("/deleteFromWatchlist", async (request, response) => {
 
   try {
     // Find the watchlist by userId
-    let watchlist = await WatchList.findOne({userId: userObjId});
+    let watchlist = await WatchList.findOne({ userId: userObjId });
     if (!watchlist) {
       logger.error("Watchlist not found");
       return response.status(404).json({
@@ -169,7 +208,9 @@ router.post("/deleteFromWatchlist", async (request, response) => {
     if (!watchlist.stocks.includes(stockObjId)) {
       logger.info("No such stock found inside watchlist to be deleted");
     } else {
-      watchlist.stocks = watchlist.stocks.filter(stock => !stock.equals(stockObjId));
+      watchlist.stocks = watchlist.stocks.filter(
+        (stock) => !stock.equals(stockObjId)
+      );
       await watchlist.save();
       logger.info("Stock successfully deleted from watchlist");
       return response.status(200).json({
