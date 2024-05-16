e7cb5acdf771b288c6e18bc02ee6fc40b51ee141
andyleezhou <andy.lee.zhou@gmail.com>
2024-05-16 14:20:42 -0700

    updating notification router

--- routes/notificationRouter.js
+++ routes/notificationRouter.js
@@ -2,33 +2,58 @@ const express = require("express");
 const router = express.Router();
 const logger = require("../config/logger");
 
-router.post('/notifications', (request, response) => {
-    // Parse the request body
-    const { title, message, userId } = request.body;
+let clients = [];
 
-    if (!title || !message || !userId) {
-        logger.error("Title, message, or userId cannot be null");
-        return response.status(400).json({
-          message: "Title, message, or userId cannot be null",
-          status: 400,
-        });
-    }
-   
-   // Send the notification to the user
-    sendNotification(userId, title, message);
-   return response.status(200).json({
-       status: 200,
-       message: 'Notification sent successfully',
-       userId
+// Endpoint for SSE
+router.get('/events', (request, response) => {
+    response.setHeader('Content-Type', 'text/event-stream');
+    response.setHeader('Cache-Control', 'no-cache');
+    response.setHeader('Connection', 'keep-alive');
+
+    // Send initial data to keep the connection open
+    response.write('retry: 10000\n\n');
+
+    clients.push(response);
+
+    // Remove client when connection is closed
+    request.on('close', () => {
+        clients = clients.filter(client => client !== response);
     });
 });
 
-const sendNotification = (userId, title, message) => {
-    // Implement new notification logic with 3rd party service here
-    // console logs are a place holder
-    console.log(`Sending notification to user ${userId}`);
+// Endpoint to send notifications
+router.post('/notifications', (request, response) => {
+    const { title, message } = request.params;
+
+    if (!title || !message) {
+        logger.error("Title, message, or userId cannot be null");
+        return response.status(400).json({
+            message: "Title, message, or userId cannot be null",
+            status: 400,
+        });
+    }
+
+    // Send the notification to the user
+    sendNotification(title, message);
+
+    return response.status(200).json({
+        status: 200,
+        message: 'Notification sent successfully',
+        userId,
+    });
+});
+
+const sendNotification = (title, message) => {
+    const notification = JSON.stringify({ title, message });
+
+    // Send notification to all connected clients
+    clients.forEach(client => {
+        client.write(`data: ${notification}\n\n`);
+    });
+
     console.log(`Title: ${title}`);
     console.log(`Message: ${message}`);
-}
+};
+
 module.exports = router;
    
